2b70830ee95fb715a73cec9726c98e307c603b0f
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-03-09 19:49:44 +0800

    Initial commit

---
+++ MutipleWindowPractice/Demonstrations/MainApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MutipleWindowPractice
+//
+//  Created by Chiaote Ni on 2024/2/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+
+    }
+}
